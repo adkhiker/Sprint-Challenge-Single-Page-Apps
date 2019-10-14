a6e0335cb50139f87b612d18832490e352f9a0f1
adkhiker <poirier.greg@gmail.com>
2019-10-13 23:04:11 -0400

    WIP: searchform

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm({ onSearch }) {
+  const [query, setQuery] = useState({
+    name: ""
+  });
+  const handleInputChange = event => {
+    setQuery({ ...query, name: event.target.value });
+  };
+
+  const search = event => {
+    onSearch(query.name);
+    event.preventDefault();
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={event => search(event)}>
+        <input
+          onChange={handleInputChange}
+          placeholder="name"
+          value={query.name}
+          name="name"
+        />
+        <button type="submit">Search</button>
+      </form>
     </section>
   );
 }
