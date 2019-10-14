6f1396610a1550a39fbb00ffa6616011c9f7ec4b
adkhiker <poirier.greg@gmail.com>
2019-10-13 22:06:41 -0400

    WIP: Styling

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, CardText, CardBody, CardTitle, CardImg, Col } from "reactstrap";
 
 export default function WelcomePage() {
   return (
@@ -6,11 +7,32 @@ export default function WelcomePage() {
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
-          className="main-img"
+          className="main-img ui centered medium circular image"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
       </header>
     </section>
+  );
+}
+
+function CharCard(props) {
+  return (
+    <Col xs="6" lg="4" height="100%">
+      <Card height="100%">
+        <CardImg src="https://source.unsplash.com/random" width="100%" top />
+        <CardBody height="100%">
+          {/*
+
+          */}
+          <CardTitle>{props.title}</CardTitle>
+          <hr />
+          <CardText>{props.description}</CardText>
+          <hr />
+          <CardText height="100%">Director: {props.director}</CardText>
+          <small>Date Released: {props.release_date}</small>
+        </CardBody>
+      </Card>
+    </Col>
   );
 }
